a856bd4c8fd2abedf935c0f3684e2d0c9a9abd69
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-08-23 20:52:59 +0700

    add CORS configuration to api-gateway to allow all origins, headers, and methods

--- api-gateway/src/main/java/com/minhkha/gateway/config/WebClientConfig.java
+++ api-gateway/src/main/java/com/minhkha/gateway/config/WebClientConfig.java
@@ -3,6 +3,9 @@ package com.minhkha.gateway.config;
 import com.minhkha.gateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -15,6 +18,20 @@ public class WebClientConfig {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/identity")
                 .build();
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 
     @Bean
